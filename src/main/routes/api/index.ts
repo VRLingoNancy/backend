204b2e15bdab361f6d3948ea3dad0bc1193b6e44
Davelooper <ddonnot@gmail.com>
2025-08-19 16:06:41 +0200

    build: add nodejs fastify application

---
+++ src/main/routes/api/index.ts
@@ -0,0 +1,13 @@
+import type { FastifyPluginAsync } from 'fastify';
+
+const indexRoute: FastifyPluginAsync = async (fastify) => {
+  fastify.get('/', async (_request, _reply) => {
+    return { message: 'Welcome to the API!' };
+  });
+
+  fastify.get('/ping', async (_request, _reply) => {
+    return 'pong\n';
+  });
+};
+
+export default indexRoute;
