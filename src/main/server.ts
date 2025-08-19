2b8fbc54a86f9899cb7e7f928057f9119d9494cc
Davelooper <ddonnot@gmail.com>
2025-08-19 16:06:41 +0200

    build: add nodejs fastify application

---
+++ src/main/server.ts
@@ -0,0 +1,16 @@
+import Fastify from 'fastify';
+import app from './app';
+
+const server = Fastify({ logger: true });
+await server.register(app);
+
+// const port = Number(process.env.PORT ?? 3000);
+const port = 4000;
+
+try {
+  await server.listen({ port, host: '0.0.0.0' })
+  console.log(`Server is running at http://localhost:${port}`);
+} catch (err) {
+  console.error('Error starting server:', err);
+  process.exit(1);
+}
