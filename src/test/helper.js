28eb8cc16347b4e464dca162fac71d35823c4468
Davelooper <ddonnot@gmail.com>
2025-08-26 21:07:11 +0200

    chore: lint all files

--- src/test/helper.js
+++ src/test/helper.js
@@ -1,11 +1,11 @@
-'use strict';
+"use strict";
 
 // This file contains code that we reuse
 // between our tests.
 
-const { build: buildApplication } = require('fastify-cli/helper');
-const path = require('node:path');
-const AppPath = path.join(__dirname, '..', 'app.js');
+const { build: buildApplication } = require("fastify-cli/helper");
+const path = require("node:path");
+const AppPath = path.join(__dirname, "..", "app.js");
 
 // Fill in this config with all the configurations
 // needed for testing the application
--- src/test/routes/example.test.js
+++ src/test/routes/example.test.js
@@ -1,16 +1,16 @@
-'use strict';
+"use strict";
 
-const { test } = require('node:test');
-const assert = require('node:assert');
-const { build } = require('../helper');
+const { test } = require("node:test");
+const assert = require("node:assert");
+const { build } = require("../helper");
 
-test('example is loaded', async (t) => {
+test("example is loaded", async (t) => {
   const app = await build(t);
 
   const res = await app.inject({
-    url: '/example',
+    url: "/example",
   });
-  assert.equal(res.payload, 'this is an example');
+  assert.equal(res.payload, "this is an example");
 });
 
 // inject callback style:
