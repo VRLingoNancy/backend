39f5ba5d228f617f80d130409ce70b413f468e7b
Davelooper <ddonnot@gmail.com>
2025-08-26 21:04:47 +0200

    chore: lint all files (not finished)

--- src/main/server.ts
+++ src/main/server.ts
@@ -1,4 +1,4 @@
-import "dotenv/config";
+import 'dotenv/config';
 import Fastify from 'fastify';
 import app from './app';
 
@@ -8,7 +8,7 @@ await server.register(app);
 const port = Number(process.env.PORT ?? 3000);
 
 try {
-  await server.listen({ port, host: '0.0.0.0' })
+  await server.listen({ port, host: '0.0.0.0' });
   console.log(`Server is running at http://localhost:${port}`);
 } catch (err) {
   console.error('Error starting server:', err);
