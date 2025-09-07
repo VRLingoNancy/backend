782e273516a3428c68a0b00a41bb401a4ebe5812
Davelooper <86955516+Davelooper@users.noreply.github.com>
2025-09-07 20:17:16 +0200

    build: add user and auth tables in db (#10)

--- src/main/routes/api/index.ts
+++ src/main/routes/api/index.ts
@@ -1,9 +1,6 @@
 import type { FastifyPluginAsync } from 'fastify';
-import testItemRoutes from './test-item';
 
 const indexRoute: FastifyPluginAsync = async (fastify) => {
-  await fastify.register(testItemRoutes);
-
   fastify.get('/', async () => {
     return { message: 'Welcome to the API!' };
   });
