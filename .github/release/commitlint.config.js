2348d5599f750f9d86c1d85aa18addc6ad23e024
Davelooper <86955516+Davelooper@users.noreply.github.com>
2025-09-07 17:02:55 +0200

    build: database initialization (#9)

--- .github/release/commitlint.config.js
+++ .github/release/commitlint.config.js
@@ -5,10 +5,25 @@ module.exports = {
     'scope-enum': [2, 'always', ['ci']],
 
     // Compliant with Angular conventions (get rid of "style" type but tolerate "chore" one)
-    'type-enum': [2, 'always', ['feat', 'fix', 'perf', 'revert', 'refactor', 'build', 'test', 'ci', 'docs', 'chore']],
+    'type-enum': [
+      2,
+      'always',
+      [
+        'feat',
+        'fix',
+        'perf',
+        'revert',
+        'refactor',
+        'build',
+        'test',
+        'ci',
+        'docs',
+        'chore',
+      ],
+    ],
 
     // Adjust some rules predefined by @commitlint/config-conventional
     'body-max-line-length': [0, 'always', 'Infinity'],
-    'footer-max-line-length': [0, 'always', 'Infinity']
-  }
-}
+    'footer-max-line-length': [0, 'always', 'Infinity'],
+  },
+};
